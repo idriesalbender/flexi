006cab26b00e788256e2556cd4d28b5c0c667c15
idriesalbender <idries_albender@hotmail.com>
2019-02-14 12:34:05 +0100

    Updates

---
+++ component---src-pages-index-js-57a8a5e2e105db703960.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{139:function(e,n,a){"use strict";a.r(n);var t=a(0),o=a.n(t),c=a(154),l=a(152),r=a(151);n.default=function(){return o.a.createElement(c.a,null,o.a.createElement(l.a,{title:"",keywords:["css","grid","flexbox","responsive","fluid","mobile-first"]}),o.a.createElement(r.a,null,o.a.createElement("p",{align:"center",className:"mdc-typography--body1"},"Docs coming soon!")))}}}]);
+//# sourceMappingURL=component---src-pages-index-js-57a8a5e2e105db703960.js.map
