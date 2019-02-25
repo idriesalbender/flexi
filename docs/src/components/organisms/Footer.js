1a00fbe0b0539049324fba12be4ea08534012d1c
idriesalbender <idries_albender@hotmail.com>
2019-02-25 23:40:29 +0100

    Create Footer component

---
+++ docs/src/components/organisms/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Heart from '../../images/heart.svg';
+import Paragraph from '../atoms/Paragraph';
+
+export default () => (
+  <footer className="mdc-typography--body2">
+    <p align="center">
+      Made with <Heart /> by idriesalbender
+    </p>
+  </footer>
+);
