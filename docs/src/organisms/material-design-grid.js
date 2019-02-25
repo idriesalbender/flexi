d4f5fd87b27fd0fc87f4bd43a2a5ed8db73a42c2
idriesalbender <idries_albender@hotmail.com>
2019-02-25 14:59:05 +0100

    Create material design section

---
+++ docs/src/organisms/material-design-grid.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Paragraph from '../components/atoms/Paragraph';
+import Phone from '../images/phone.svg';
+import Tablet from '../images/tablet.svg';
+import Image from '../components/atoms/Image';
+
+export default () => (
+  <>
+    <h1 className="mdc-typography--headline3 mdc-theme--secondary">Material Design Grid</h1>
+    <Paragraph>
+      The Flexi grid is based on Material Design guidelines. This means that gutter size will
+      differ according to screen width.
+    </Paragraph>
+    <Paragraph>
+      By default, a <code>1rem</code> (16px) gutter is used. At the <code>≥45em</code> (720px)
+      breakpoint, gutter size becomes <code>1.5rem</code> (24px).
+    </Paragraph>
+    <Image description="On a mobile device (@411px), 1rem gutters are used.">
+      <Phone width="100%" height="auto" />
+    </Image>
+    <Image description="On a larger tablet (@768px), 1.5rem gutters are used.">
+      <Tablet width="100%" height="auto" />
+    </Image>
+  </>
+);
