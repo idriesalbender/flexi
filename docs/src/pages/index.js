cd9aff74a4e70c8fe2f1e59b26c986b78169d15b
idriesalbender <idries_albender@hotmail.com>
2019-02-25 23:27:34 +0100

    Add intro text

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -7,6 +7,7 @@ import Grid from '../components/atoms/Grid';
 import Column from '../components/atoms/Column';
 
 import MaterialDesignGrid from '../organisms/material-design-grid';
+import Usage from '../organisms/usage';
 
 const IndexPage = () => (
   <Layout>
@@ -17,6 +18,12 @@ const IndexPage = () => (
     <Container>
       <Grid>
         <Column xs={12}>
+          <h6 className="mdc-typography--headline6">
+            Flexi is a mobile-first, lightweight CSS grid system based on the flexbox spec,
+            taking some cues from Material Design guidelines regarding breakpoints and grid
+            spacing/gutters.
+          </h6>
+          <Usage />
           <MaterialDesignGrid />
         </Column>
       </Grid>
